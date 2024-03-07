3132cb8095512b5ec058f392d8ca9d7973154f90
DRAKINO 987 <vincentkarl12@gmail.com>
2024-03-07 22:21:34 +0800

    Uploaded template files for flask implementation; partially functional

---
+++ website/templates/home.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+        .header-container {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            margin-bottom: 20px;
+        }
+
+        .links-container a {
+            padding: 10px;
+            margin: 0 10px; 
+            background-color: LightBlue;
+            position: center;
+        }
+
+        .padding {
+            padding: 30px;
+            background-color: LightBlue;
+            border-radius: 10px;
+        }
+        </style>
+    <body class="body">
+        <body style="background-color: LightCyan;">
+        <center><h1><img src="petcare_logo.png" alt="Logo" class="logo" width="150" height="150"> <br> Waggle & Snuggle Lodge</h1> <h3>Daycare and Overnight care</h3></center>
+        <div style="padding: 30px; background-color: LightBlue; border-radius:10px;">
+        <div class="links-container">
+        <center><a href="daycare_appointment.php">Day Care Appointment</a>
+        <a href="overnightcare_appointment.php">Overnight Care Appointment</a></center>
+    </div>
+    </body>
+</html>
