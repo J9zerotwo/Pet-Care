3f5c876b2f2a8159706c7ccf23c10f7fccedea76
Jeninee <102421301+J9zerotwo@users.noreply.github.com>
2024-04-05 10:58:06 +0800

    Add files via upload

--- pet-care/website/views.py
+++ pet-care/website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, app, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
 from .models import Appointment
 from datetime import datetime
@@ -10,7 +10,7 @@ views = Blueprint('views', __name__)
 def home():
     return render_template("home.html", user=current_user)
 
-@views.route('/appointment', methods=['GET', 'POST'])
+@views.route('/appointment', methods=['GET', 'POST', ''])
 @login_required
 def appointment():
     if request.method == 'POST':
@@ -32,21 +32,35 @@ def appointment():
 
         if not pet_name or not pet_age or not pet_type or not pet_gender or not pet_color:
             flash('Please fill in all the fields', category='error')
+
         else:
-            new_appointment = Appointment(
-                pet_name=pet_name,
-                pet_age=pet_age,
-                pet_type=pet_type,
-                pet_gender=pet_gender,
-                pet_color=pet_color,
-                drop_off_date=drop_off_date,
-                pick_up_date=pick_up_date,
-                user_id=current_user.id
-            )
-            db.session.add(new_appointment)
-            db.session.commit()
-            flash('Appointment successfully created!', category='success')
-            return redirect('/appointment')
+            # Check if there's an existing appointment for the same pet on the same day
+            existing_appointment = Appointment.query.filter_by(pet_name=pet_name, drop_off_date=drop_off_date).first()
+            if existing_appointment:
+                flash('An appointment for this pet already exists on this date.', category='error')
+            else:
+                new_appointment = Appointment(
+                    pet_name=pet_name,
+                    pet_age=pet_age,
+                    pet_type=pet_type,
+                    pet_gender=pet_gender,
+                    pet_color=pet_color,
+                    drop_off_date=drop_off_date,
+                    pick_up_date=pick_up_date,
+                    user_id=current_user.id
+                )
+                db.session.add(new_appointment)
+                db.session.commit()
+                flash('Appointment successfully created!', category='success')
+                return redirect('/appointment')
 
     appointments = Appointment.query.filter_by(user_id=current_user.id).all()
     return render_template('appointment.html', user=current_user, appointments=appointments)
+
+
+@views.route('/cancel/<int:id>', methods=['POST'])
+def cancel_appointment(id):
+    appointment = Appointment.query.get_or_404(id)
+    db.session.delete(appointment)
+    db.session.commit()
+    return redirect('/appointment')
